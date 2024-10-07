52f507352bcc09e16bcadcb36c156950f0887abf
Rani0086 <shantidhaakad@gmail.com>
2024-10-07 07:30:44 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -111,11 +111,11 @@ SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/mlohvdryj"
 )  # Example:- https://t.me/VIP_CREATORS
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/lolpagalokigc"
+    "SUPPORT_GROUP", "https://t.me/Crunchyrol_Anime_In_Hindi_India"
 )  # Example:- https://t.me/TG_FRIENDSS
 
 SUPPORT_CHAT = getenv(
-    "SUPPORT_GROUP", "https://t.me/lolpagalokigc"
+    "SUPPORT_GROUP", "https://t.me/Crunchyrol_Anime_In_Hindi_India"
 )  # Example:- https://t.me/TG_FRIENDSS
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
